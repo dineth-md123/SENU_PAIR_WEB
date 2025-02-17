64ac26447de0c641830442a07a8e9cdff9a2c55d
dineth-md123 <dinethsenura400@gmail.com>
2025-02-17 16:58:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   EMAIL: process.env.EMAIL || "",
   PASS: process.env.PASS || "",
   MESSAGE: process.env.MESSAGE || "*SENU-MD SESSION ID BELOW*",
-  IMAGE: process.env.IMAGE || "",
+  IMAGE: process.env.IMAGE || "https://i.ibb.co/1t4w6Q5N/5766.jpg",
 }
