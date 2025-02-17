937924e45c9768b231960365b8f4a125a8546faa
dineth-md123 <dinethsenura400@gmail.com>
2025-02-17 17:03:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "SENU-MD-V1",
-  EMAIL: process.env.EMAIL || "",
-  PASS: process.env.PASS || "",
+  EMAIL: process.env.EMAIL || "sdineth574@gmail.com",
+  PASS: process.env.PASS || "senuradineth123",
   MESSAGE: process.env.MESSAGE || "*SENU-MD SESSION ID BELOW*",
   IMAGE: process.env.IMAGE || "https://i.ibb.co/1t4w6Q5N/5766.jpg",
 }
