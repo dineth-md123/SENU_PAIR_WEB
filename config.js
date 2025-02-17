17135304b183009fe5df51bc499fc5836f1c2b9e
dineth-md123 <dinethsenura400@gmail.com>
2025-02-17 16:40:27 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "SENU-MD-V1",
+  EMAIL: process.env.EMAIL || "",
+  PASS: process.env.PASS || "",
+  MESSAGE: process.env.MESSAGE || "*SENU-MD SESSION ID BELOW*",
+  IMAGE: process.env.IMAGE || "",
+}
